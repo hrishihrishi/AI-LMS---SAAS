95816d253d0f118c7c0b0ac42d11cd9679b46bfe
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-01-12 21:36:41 +0530

    Complete voice AI setup

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,31 @@
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
 import React from 'react'
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async({searchParams}: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({subject, topic});
+
+  console.log(companions);
+
   return (
-    <div>CompanionsLibrary</div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companions</h1>
+        <SearchInput/>
+        <SubjectFilter/>
+      </section>
+      <section>
+        {companions.map((companion)=>(
+          <CompanionCard key={companion._id} {...companion}/>
+        ))}
+      </section>
+    </main>
   )
 }
 
